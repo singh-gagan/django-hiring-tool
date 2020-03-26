6ac328ead4316abedf8c0d83369a34c1f2407254
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-03-26 12:39:31 +0530

    Submission model created

--- hiringapp/models.py
+++ hiringapp/models.py
@@ -1,5 +1,19 @@
 from django.db import models
-
+from .utils import ActivityStatus
 # Create your models here.
 
+class Submission(models.Model):
 
+    candidate_name=models.CharField(max_length=200)
+    candidate_email=models.EmailField(max_length = 254)
+    activity_duration=models.TimeField(auto_now=False,auto_now_add=False)
+    activity_start_time=models.DateTimeField(blank=True,null=True)
+    activity_drive_link= models.URLField(max_length = 500)
+    activity_start_link= models.URLField(max_length = 500,blank=True,null=True)
+    activity_solution_link= models.URLField(max_length = 500,blank=True,null=True)
+    reminder_for_submission_time=models.TimeField(auto_now=False,auto_now_add=False)
+    activity_status=models.CharField(
+        max_length = 500,
+        choices=[(tag, tag.value) for tag in ActivityStatus],
+        default=ActivityStatus.Not_Yet_Started
+    )
---
+++ hiringapp/utils.py
@@ -0,0 +1,6 @@
+from enum import Enum
+class ActivityStatus(Enum):
+        Started='started'
+        Submitted='submitted'
+        Not_Yet_Started='not_yet_started'
+        Expired='expired'
