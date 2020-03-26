e3e61cfc656cd7aa05677dd7fde52ad47a161394
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-03-26 10:15:11 +0530

    setup app

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hiringapp',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
