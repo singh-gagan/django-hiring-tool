2a7f863320832f2a33a0f761859c700243df467e
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-03-31 16:45:42 +0530

    changed model field

--- hiringapp/models.py
+++ hiringapp/models.py
@@ -13,7 +13,7 @@ class Submission(models.Model):
     activity_duration=models.TimeField(auto_now=False,auto_now_add=False)
     activity_start_time=models.DateTimeField(blank=True,null=True)
     activity_drive_link= models.URLField(max_length = 500)
-    activity_uuid_link= models.UUIDField(primary_key = True, default = uuid.uuid4())
+    activity_uuid= models.UUIDField(primary_key = True, default = uuid.uuid4())
     activity_solution_link= models.URLField(max_length = 500,blank=True,null=True)
     reminder_for_submission_time=models.TimeField(auto_now=False,auto_now_add=False)
     activity_status=models.CharField(
