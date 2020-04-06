5a116bffb7e6c684812005819fca55c5cff1699c
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-06 23:04:20 +0530

    urls test init

--- hiringapp/tests/test_urls.py
+++ hiringapp/tests/test_urls.py
@@ -1,6 +1,8 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
+import uuid
 
 class TestUrls(SimpleTestCase):
     def test_submission_invite_url_resolved(self):
-        assert 1 == 2
+        url=reverse('submission_invite',args=(uuid.uuid4,))
+        print(resolve(url))
