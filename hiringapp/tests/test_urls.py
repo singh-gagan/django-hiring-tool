1fb14fe2b64296821d6537bf9e84fb2472eefd92
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-06 20:27:50 +0530

    testing initiated

---
+++ hiringapp/tests/test_urls.py
@@ -0,0 +1,6 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+class TestUrls(SimpleTestCase):
+    def test_submission_invite_url_resolved(self):
+        assert 1 == 2
