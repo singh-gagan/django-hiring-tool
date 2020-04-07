e6dc88c7f9e559bbbebab693c13b0e1da0c47df3
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-07 17:13:25 +0530

    indentation fixed

--- hiringapp/mailutils.py
+++ hiringapp/mailutils.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 from oauth2client.contrib import xsrfutil
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.contrib.django_util.storage import DjangoORMStorage
-from django.shortcuts import render, redirect
+from django.shortcuts import render,redirect
 from httplib2 import Http
 from apiclient import errors
 from email.mime.text import MIMEText
@@ -18,68 +18,57 @@ from django.views.generic.edit import FormView
 from mysite import settings
 from .utils import FLOW
 
-
 def send_message(service, user_id, message):
     try:
         message = (service.users().messages().send(userId=user_id, body=message)
-                   .execute())
+               .execute())
         print('Message Id: %s' % message['id'])
         return message
     except errors.HttpError as error:
-        print('An error occurred: %s' % error)
+        print('An error occurred: %s' % error)    
 
 
 def get_mail_service(user):
-    storage = DjangoORMStorage(
-        models.CredentialsModel, 'id', user, 'credential')
+    storage = DjangoORMStorage(models.CredentialsModel, 'id', user, 'credential')
     credential = storage.get()
     if credential is None or credential.invalid:
-        FLOW.params['state'] = xsrfutil.generate_token(
-            settings.SECRET_KEY, user)
+        FLOW.params['state'] = xsrfutil.generate_token(settings.SECRET_KEY,user)
         authorize_url = FLOW.step1_get_authorize_url()
         return HttpResponseRedirect(authorize_url)
-    service = build('gmail', 'v1', credentials=credential,
-                    cache_discovery=False)
+    service = build('gmail', 'v1', credentials=credential,cache_discovery=False)
     return service
 
-
 def get_user_emailaddress(submission):
-    service = get_mail_service(submission.invitation_host)
+    service=get_mail_service(submission.invitation_host)
     profile = service.users().getProfile(userId='me').execute()
     return profile['emailAddress']
 
 
-def create_messages(submission, email_type):
-    # logic to create message from submission details and email_type
-    mail = models.MailModel.objects.get(mail_type=email_type)
-    message = mail.mail_content
-    mail_body = create_mail_body(submission, email_type, message)
-    message = MIMEText(mail_body, 'html')
-    if email_type == 'activity_expired' or email_type == 'activity_solution':
-        message['to'] = get_user_emailaddress(submission)
-    else:
-        message['to'] = submission.candidate_email
-    message['subject'] = mail.mail_subject
+def create_messages(submission,email_type):
+    #logic to create message from submission details and email_type 
+    mail=models.MailModel.objects.get(mail_type=email_type)
+    message=mail.mail_content
+    mail_body=create_mail_body(submission,email_type,message)
+    message=MIMEText(mail_body,'html')
+    if email_type=='activity_expired' or email_type=='activity_solution':
+        message['to']=get_user_emailaddress(submission)
+    else:    
+        message['to']=submission.candidate_email
+    message['subject']=mail.mail_subject
     return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
 
-
-def create_mail_body(submission, email_type, message):
-    mail_body = ""
-    activity_invite_url = "127.0.0.1:8000" + \
-        reverse('submission_invite', args=(submission.activity_uuid,))
-    if email_type == 'reminder':
-        mail_body = message.format(candidate_name=submission.candidate_name,
-                                   activity_duration=submission.activity_duration, activity_url=activity_invite_url)
-    elif email_type == 'invitation':
-        mail_body = message.format(candidate_name=submission.candidate_name,
-                                   activity_duration=submission.activity_duration, activity_url=activity_invite_url)
-    elif email_type == 'reminder_to_submit':
-        mail_body = message.format(candidate_name=submission.candidate_name,
-                                   time_left=submission.reminder_for_submission_time, activity_url=activity_invite_url)
-    elif email_type == 'activity_expired':
-        mail_body = message.format(candidate_name=submission.candidate_name,
-                                   candidate_email=submission.candidate_email, activity_start_time=submission.activity_start_time)
-    elif email_type == 'activity_solution':
-        mail_body = message.format(candidate_name=submission.candidate_name,
-                                   candidate_email=submission.candidate_email, activity_solution=submission.activity_solution_link)
+def create_mail_body(submission,email_type,message):
+    mail_body=""
+    activity_invite_url="127.0.0.1:8000"+reverse('submission_invite',args=(submission.activity_uuid,))
+    if email_type=='reminder':
+        mail_body=message.format(candidate_name=submission.candidate_name,activity_duration=submission.activity_duration,activity_url=activity_invite_url)
+    elif email_type=='invitation':
+        mail_body=message.format(candidate_name=submission.candidate_name,activity_duration=submission.activity_duration,activity_url=activity_invite_url)
+    elif email_type=='reminder_to_submit':
+        mail_body=message.format(candidate_name=submission.candidate_name,time_left=submission.reminder_for_submission_time,activity_url=activity_invite_url)
+    elif email_type=='activity_expired':
+        mail_body=message.format(candidate_name=submission.candidate_name,candidate_email=submission.candidate_email,activity_start_time=submission.activity_start_time)
+    elif email_type=='activity_solution':
+        mail_body=message.format(candidate_name=submission.candidate_name,candidate_email=submission.candidate_email,activity_solution=submission.activity_solution_link)    
     return mail_body
+        
--- hiringapp/models.py
+++ hiringapp/models.py
@@ -1,86 +1,78 @@
 from django.db import models
 from django.contrib import admin
-from .utils import ActivityStatus, EmailType
+from .utils import ActivityStatus,EmailType
 import uuid
-from django.contrib.auth.models import User
-from oauth2client.contrib.django_util.models import CredentialsField
+from django.contrib.auth.models import User 
+from oauth2client.contrib.django_util.models import CredentialsField 
 from django.utils import timezone
 from .tasks import send_emails
 import datetime
 from django.core.exceptions import ValidationError
 
-
 class Submission(models.Model):
 
-    # Candidate's related Info
-    candidate_name = models.CharField(max_length=200)
-    candidate_email = models.EmailField(max_length=254)
+    #Candidate's related Info
+    candidate_name=models.CharField(max_length=200)
+    candidate_email=models.EmailField(max_length = 254)
 
-    # Activity Realted Info
-    activity_duration = models.DurationField(
-        default=datetime.timedelta(days=2, hours=0))
-    activity_start_time = models.DateTimeField(
-        blank=True, null=True, editable=False)
-    activity_drive_link = models.URLField(max_length=500)
-    activity_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    activity_solution_link = models.URLField(
-        max_length=500, blank=True, null=True, editable=False)
-    activity_status = models.CharField(
-        max_length=500,
+    #Activity Realted Info
+    activity_duration=models.DurationField(default=datetime.timedelta(days=2, hours=0))
+    activity_start_time=models.DateTimeField(blank=True,null=True,editable=False)
+    activity_drive_link= models.URLField(max_length = 500)
+    activity_uuid= models.UUIDField(primary_key = True, default = uuid.uuid4)
+    activity_solution_link= models.URLField(max_length = 500,blank=True,null=True,editable=False)
+    activity_status=models.CharField(
+        max_length = 500,
         choices=[(key.value, key.name) for key in ActivityStatus],
         default=ActivityStatus.Not_Yet_Started,
     )
 
-    # remainder mails related Info
-    reminder_for_submission_time = models.DurationField(
-        default=datetime.timedelta(days=0, hours=2))
+    #remainder mails related Info
+    reminder_for_submission_time=models.DurationField(default=datetime.timedelta(days=0,hours=2))
 
-    # Invitation realted Info who and when
-    invitation_host = models.ForeignKey(
-        User, on_delete=models.CASCADE, editable=False, blank=True, null=True)
-    invitation_creation_dateandtime = models.DateTimeField(
-        editable=False, blank=True, null=True)
+    #Invitation realted Info who and when
+    invitation_host=models.ForeignKey(User,on_delete=models.CASCADE,editable=False,blank=True,null=True)
+    invitation_creation_dateandtime=models.DateTimeField(editable=False,blank=True,null=True)
+    
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs): 
         if self._state.adding is True:
-            id = self.activity_uuid
-            send_emails.delay(id, 'invitation')
-        super(Submission, self).save(*args, **kwargs)
+            id=self.activity_uuid
+            send_emails.delay(id,'invitation')
+        super(Submission, self).save(*args, **kwargs) 
 
     def __str__(self):
-        str = "Invitation to {}".format(self.candidate_name)
+        str="Invitation to {}".format(self.candidate_name)
         return str
 
+class CredentialsModel(models.Model): 
+    id = models.ForeignKey(User, primary_key = True, on_delete = models.CASCADE) 
+    credential = CredentialsField() 
 
-class CredentialsModel(models.Model):
-    id = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
-    credential = CredentialsField()
-
-
-class CredentialsAdmin(admin.ModelAdmin):
+class CredentialsAdmin(admin.ModelAdmin): 
     pass
 
 
 class MailModel(models.Model):
-    mail_type = models.CharField(
+    mail_type=models.CharField(
         max_length=100,
         choices=[(key.value, key.name) for key in EmailType],
         unique=True,
         default=EmailType.Invitation,
     )
-    mail_subject = models.CharField(max_length=100)
-    mail_content = models.CharField(max_length=1000)
+    mail_subject=models.CharField(max_length=100)
+    mail_content=models.CharField(max_length=1000)
 
     def __str__(self):
         return self.mail_type
 
 
 class MailSummary(models.Model):
-    mail_type = models.CharField(
+    mail_type=models.CharField(
         max_length=100,
         choices=[(key.value, key.name) for key in EmailType],
         default=EmailType.Invitation,
     )
-    activity_uuid = models.UUIDField(null=True)
-    candidate_name = models.CharField(max_length=200)
-    date_of_mail = models.DateTimeField(blank=True, null=True)
+    activity_uuid=models.UUIDField(null=True)
+    candidate_name=models.CharField(max_length=200)
+    date_of_mail=models.DateTimeField(blank=True,null=True)
