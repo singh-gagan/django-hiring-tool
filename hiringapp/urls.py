0a1b8fe4be7c88e7e784c5f2e2f4ec8a36005b93
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-07 10:17:11 +0530

    urls repath and onetoone

--- hiringapp/urls.py
+++ hiringapp/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from . import views
 
 urlpatterns=[
@@ -7,6 +7,6 @@ urlpatterns=[
     path('authenticate/',views.Gmail_Authenticate.as_view(),name='gmail_authenticate'),
     path('admin/oauth2callback',views.Auth_Return.as_view(),name='oauth2callback'),
     path('logout/',views.Log_Out.as_view(),name='log_out'),
-    path('submission_invite/(?P<activity_uuid>[0-9a-f-]+)',views.SubmissionInvite.as_view(),name='submission_invite'),
-    path('submission_solution/(?P<activity_uuid>[0-9a-f-]+)',views.SubmitSolution.as_view(),name='submission_solution'),
+    re_path('submission_invite/(?P<activity_uuid>[0-9a-f-]+)',views.SubmissionInvite.as_view(),name='submission_invite'),
+    re_path('submission_solution/(?P<activity_uuid>[0-9a-f-]+)',views.SubmitSolution.as_view(),name='submission_solution'),
 ]
