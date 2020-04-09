e6b46af734cafa7d9fe20d5712f07bf1c2887c14
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-09 19:06:16 +0530

    changes variable input_solution_link

--- hiringapp/views.py
+++ hiringapp/views.py
@@ -90,7 +90,7 @@ class SubmissionInviteView(View):
 class SubmitSolutionView(View):
 
     def post(self,request,activity_uuid):
-        solution_link_input = request.POST['solution_link']
+        input_solution_link = request.POST['solution_link']
         submission=Submission.get_submission(activity_uuid)
         if submission is None:
             return render(request,'hiringapp/display_activity.html',{'invalid':True})
@@ -98,7 +98,7 @@ class SubmitSolutionView(View):
             messages.error(request, 'You can submit solution only once.')
             return HttpResponseRedirect(reverse('submission_invite',args=(submission.activity_uuid,)))    
         submission.activity_status=ActivityStatus.Submitted.value
-        submission.activity_solution_link=solution_link_input
+        submission.activity_solution_link=input_solution_link
         submission.save(update_fields=["activity_status","activity_solution_link",])
         send_emails.delay(submission.activity_uuid,EmailType.ActivitySolution.value)
         return HttpResponseRedirect(reverse('submission_invite',args=(submission.activity_uuid,)))
