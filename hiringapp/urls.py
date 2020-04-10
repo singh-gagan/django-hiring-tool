0aafacb0b58bc44a1f4f2c3137bea73db86d19d9
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-10 10:59:21 +0530

    moved admin urls from app to site

--- hiringapp/urls.py
+++ hiringapp/urls.py
@@ -3,10 +3,9 @@ from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('authenticate/', views.GmailAuthenticateView.as_view(),
          name='gmail_authenticate'),
-    path('admin/oauth2callback', views.GmailAuthReturnView.as_view(), name='oauth2callback'),
+    path('oauth2callback', views.GmailAuthReturnView.as_view(), name='oauth2callback'),
     re_path('submission_invite/(?P<activity_uuid>[0-9a-f-]+)',
             views.SubmissionInviteView.as_view(), name='submission_invite'),
     re_path('submission_solution/(?P<activity_uuid>[0-9a-f-]+)',
