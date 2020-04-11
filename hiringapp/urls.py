ed45abd587a6ce406a4664aec128491db562c21c
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-12 02:01:47 +0530

    Urls of hiringapp resolved

--- hiringapp/urls.py
+++ hiringapp/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, include, re_path
 from . import views
 from mailingapp import views as mailingapp_views
 
 urlpatterns = [
-    path('authenticate/', mailingapp_views.GmailAuthenticateView.as_view(),
+    url(r'^authenticate/$', mailingapp_views.GmailAuthenticateView.as_view(),
          name='gmail_authenticate'),
-    path('oauth2callback', mailingapp_views.GmailAuthReturnView.as_view(), name='oauth2callback'),
-    re_path('submission_invite/(?P<activity_uuid>[0-9a-f-]+)',
+    url(r'^oauth2callback/$', mailingapp_views.GmailAuthReturnView.as_view(), name='oauth2callback'),
+    url(r'^invite-acivity/(?P<activity_uuid>[0-9a-f-]+)/$',
             views.SubmissionInviteView.as_view(), name='submission_invite'),
-    re_path('submission_solution/(?P<activity_uuid>[0-9a-f-]+)',
+    url(r'^solution-activity/(?P<activity_uuid>[0-9a-f-]+)/$',
             views.SubmitSolutionView.as_view(), name='submission_solution'),
 ]
