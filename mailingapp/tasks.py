7dc28e253505fc8cafd61e2397433a511786f6ee
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-13 14:24:44 +0530

    divided checkout pending tasks

--- mailingapp/tasks.py
+++ mailingapp/tasks.py
@@ -24,29 +24,36 @@ def send_emails(activity_uuid,email_type):
 
 @shared_task
 def checkout_pending_tasks():
-    # This schedulded method will iterate for every submission objects and check for pending tasks of sending emails
-    # All reminders mail will be sent in accordance with the pattern given in reminders_gap_list[]
     from hiringapp.models import Submission
     current_date=timezone.now().date()
     reminders_gap_list=[1,3,6]
     all_submissions=Submission.objects.all()
     for submission in all_submissions:
         if submission.activity_status == ActivityStatus.NOTYETSTARTED.value:
-            latest_mail_sent_date=MailSummary.get_latest_mail_sent_date(submission)
-            if current_date==latest_mail_sent_date:
-                continue
-            gap=current_date-submission.invitation_creation_dateandtime.date()
-            if gap.days in reminders_gap_list:
-                send_emails.delay(submission.activity_uuid,EmailType.STARTREMINDER.value)
-        elif submission.activity_status == ActivityStatus.STARTED.value and timezone.now()<=submission.end_time:
-            latest_mail_sent_type=MailSummary.get_latest_mail_sent_type(submission)
-            if latest_mail_sent_type==EmailType.SUBMISSIONREMINDER.value: 
-                continue
-            activity_reminder_time=submission.end_time-submission.reminder_for_submission_time
-            if timezone.now()>=activity_reminder_time:
-                send_emails.delay(submission.activity_uuid,EmailType.SUBMISSIONREMINDER.value)
-        elif submission.activity_status == ActivityStatus.STARTED.value and timezone.now()>submission.end_time:
-            submission.activity_status=ActivityStatus.EXPIRED.value
-            submission.save(update_fields=["activity_status"])
-            send_emails.delay(submission.activity_uuid,EmailType.ACTIVITYEXPIRED.value)
+           check_for_reminder_to_start_mails(submission,reminders_gap_list)
+        elif submission.activity_status == ActivityStatus.STARTED.value:
+            check_for_reminder_to_submit_or_expiry_mails(submission)
 
+
+def check_for_reminder_to_start_mails(submission,reminders_gap_list):
+    current_date=timezone.now().date()
+    latest_mail_sent_date=MailSummary.get_latest_mail_sent_date(submission)
+    if current_date==latest_mail_sent_date:
+        return
+    gap=current_date-submission.invitation_creation_dateandtime.date()
+    if gap.days in reminders_gap_list:
+        send_emails.delay(submission.activity_uuid,EmailType.STARTREMINDER.value)
+
+
+def check_for_reminder_to_submit_or_expiry_mails(submission):
+    if timezone.now()<=submission.end_time:
+        latest_mail_sent_type=MailSummary.get_latest_mail_sent_type(submission)
+        if latest_mail_sent_type==EmailType.SUBMISSIONREMINDER.value: 
+            return
+        activity_reminder_time=submission.end_time-submission.reminder_for_submission_time
+        if timezone.now()>=activity_reminder_time:
+            send_emails.delay(submission.activity_uuid,EmailType.SUBMISSIONREMINDER.value)
+    else:
+        submission.activity_status=ActivityStatus.EXPIRED.value
+        submission.save(update_fields=["activity_status"])
+        send_emails.delay(submission.activity_uuid,EmailType.ACTIVITYEXPIRED.value)
