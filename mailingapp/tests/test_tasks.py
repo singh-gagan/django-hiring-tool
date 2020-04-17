9c9667fbbd8d3b0c4b56d4554e84e1ea82c3f08b
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-17 16:31:03 +0530

    applied black on mailingapp tests

--- mailingapp/tests/test_tasks.py
+++ mailingapp/tests/test_tasks.py
@@ -6,21 +6,28 @@ from django.utils import timezone
 
 from hiringapp.constants import ActivityStatus
 from hiringapp.models import Submission
-from mailingapp.models import EmailLog, EmailTemplate, EmailType
+from mailingapp.models import EmailLog
 from mailingapp.tasks import checkout_pending_tasks
 
 
 class TestTasks(TestCase):
-
-    @patch('mailingapp.tasks.send_emails.delay')
-    def test_checkout_pending_tas_to_send_reminders_to_start_mails(self, mocked_send_emails):
+    @patch("mailingapp.tasks.send_emails.delay")
+    def test_checkout_pending_tas_to_send_reminders_to_start_mails(
+        self, mocked_send_emails
+    ):
         # Creating submission objects having gap of 1-7 days between invitation creation datetime and todays date.
-        # Given gap pattern is [1,3,6] so out of this 6 objects going to be created 3 objects are eligible for reminders to start the activity
+        # Given gap pattern is [1,3,6] so out of these 6 objects created
+        # only 3 objects are eligible for reminders to start the activity.
         for day in range(1, 7):
             submission = Submission.objects.create(
-                invitation_creation_dateandtime=timezone.now()-datetime.timedelta(days=day))
-            EmailLog.objects.create(activity_uuid=submission.activity_uuid,
-                                    date_of_mail=submission.invitation_creation_dateandtime, mail_status='SENT')
+                invitation_creation_dateandtime=timezone.now()
+                - datetime.timedelta(days=day)
+            )
+            EmailLog.objects.create(
+                activity_uuid=submission.activity_uuid,
+                date_of_mail=submission.invitation_creation_dateandtime,
+                mail_status="SENT",
+            )
 
         # 6 invitaion mails after creating submission objects
         self.assertEqual(mocked_send_emails.call_count, 6)
@@ -29,12 +36,19 @@ class TestTasks(TestCase):
         # 3 more reminders to start which are in the gap of 1,3 and 6
         self.assertEqual(mocked_send_emails.call_count, 9)
 
-    @patch('mailingapp.tasks.send_emails.delay')
-    def test_checkout_pending_task_to_send_reminder_to_submit_mails(self, mocked_send_emails):
+    @patch("mailingapp.tasks.send_emails.delay")
+    def test_checkout_pending_task_to_send_reminder_to_submit_mails(
+        self, mocked_send_emails
+    ):
         submission = Submission.objects.create(
-            invitation_creation_dateandtime=timezone.now()-datetime.timedelta(days=1, hours=22))
-        EmailLog.objects.create(activity_uuid=submission.activity_uuid,
-                                date_of_mail=submission.invitation_creation_dateandtime, mail_status='SENT')
+            invitation_creation_dateandtime=timezone.now()
+            - datetime.timedelta(days=1, hours=22)
+        )
+        EmailLog.objects.create(
+            activity_uuid=submission.activity_uuid,
+            date_of_mail=submission.invitation_creation_dateandtime,
+            mail_status="SENT",
+        )
 
         submission.activity_start_time = submission.invitation_creation_dateandtime
         submission.activity_status = ActivityStatus.STARTED.value
@@ -44,12 +58,17 @@ class TestTasks(TestCase):
 
         self.assertEqual(mocked_send_emails.call_count, 2)
 
-    @patch('mailingapp.tasks.send_emails.delay')
-    def test_checkout_pending_task_to_send_expiry_mails_to_admin(self, mocked_send_emails):
+    @patch("mailingapp.tasks.send_emails.delay")
+    def test_checkout_pending_task_to_send_expiry_mails_to_admin(
+        self, mocked_send_emails
+    ):
         submission = Submission.objects.create(
-            invitation_creation_dateandtime=timezone.now()-datetime.timedelta(days=2))
-        EmailLog.objects.create(activity_uuid=submission.activity_uuid,
-                                date_of_mail=submission.invitation_creation_dateandtime)
+            invitation_creation_dateandtime=timezone.now() - datetime.timedelta(days=2)
+        )
+        EmailLog.objects.create(
+            activity_uuid=submission.activity_uuid,
+            date_of_mail=submission.invitation_creation_dateandtime,
+        )
 
         submission.activity_start_time = submission.invitation_creation_dateandtime
         submission.activity_status = ActivityStatus.STARTED.value
@@ -60,5 +79,4 @@ class TestTasks(TestCase):
         submission.refresh_from_db()
 
         self.assertEqual(mocked_send_emails.call_count, 2)
-        self.assertEqual(submission.activity_status,
-                         ActivityStatus.EXPIRED.value)
+        self.assertEqual(submission.activity_status, ActivityStatus.EXPIRED.value)
--- mailingapp/tests/test_urls.py
+++ mailingapp/tests/test_urls.py
@@ -1,13 +1,12 @@
 from django.urls import resolve, reverse
-
 from mailingapp.views import GmailAuthCallbackView, GmailAuthenticateView
 
 
 def test_authenticate_url_resolved(self):
-    url = reverse('gmail_authenticate')
+    url = reverse("gmail_authenticate")
     self.assertEqual(resolve(url).func.view_class, GmailAuthenticateView)
 
 
 def test_oauth2callback_url_resolved(self):
-    url = reverse('oauth2callback')
+    url = reverse("oauth2callback")
     self.assertEqual(resolve(url).func.view_class, GmailAuthCallbackView)
