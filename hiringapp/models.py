f624dc9c826a59289a162c1331f266c85631efcf
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-17 14:33:05 +0530

    applied black on models,constants,urls

--- hiringapp/models.py
+++ hiringapp/models.py
@@ -20,14 +20,16 @@ class Submission(models.Model):
 
     # Activity Realted Info
     activity_duration = models.DurationField(
-        default=datetime.timedelta(days=2, hours=0))
-    activity_start_time = models.DateTimeField(
-        blank=True, null=True, editable=False)
+        default=datetime.timedelta(days=2, hours=0)
+    )
+    activity_start_time = models.DateTimeField(blank=True, null=True, editable=False)
     activity_drive_link = models.URLField(max_length=500, blank=False)
     activity_uuid = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False)
+        primary_key=True, default=uuid.uuid4, editable=False
+    )
     activity_solution_link = models.URLField(
-        max_length=500, blank=True, null=True, editable=False)
+        max_length=500, blank=True, null=True, editable=False
+    )
     activity_status = models.CharField(
         max_length=500,
         choices=[(key.value, key.name) for key in ActivityStatus],
@@ -36,13 +38,16 @@ class Submission(models.Model):
 
     # remainder mails related Info
     reminder_for_submission_time = models.DurationField(
-        default=datetime.timedelta(days=0, hours=2))
+        default=datetime.timedelta(days=0, hours=2)
+    )
 
     # Invitation realted Info who and when
     invitation_host = models.ForeignKey(
-        User, on_delete=models.CASCADE, editable=False, blank=True, null=True)
+        User, on_delete=models.CASCADE, editable=False, blank=True, null=True
+    )
     invitation_creation_dateandtime = models.DateTimeField(
-        editable=False, blank=True, null=True)
+        editable=False, blank=True, null=True
+    )
 
     def save(self, *args, **kwargs):
         if self._state.adding is True:
@@ -63,17 +68,23 @@ class Submission(models.Model):
 
     @property
     def activity_end_time(self):
-        return (None if self.activity_start_time is None
-                else (self.activity_start_time+self.activity_duration))
+        return (
+            None
+            if self.activity_start_time is None
+            else (self.activity_start_time + self.activity_duration)
+        )
 
     @property
     def activity_left_time(self):
-        return (None if self.activity_end_time is None
-                else (self.activity_end_time-timezone.now()))
+        return (
+            None
+            if self.activity_end_time is None
+            else (self.activity_end_time - timezone.now())
+        )
 
     @classmethod
     def get_admin_change_list_url(cls):
-        return reverse('admin:hiringapp_submission_changelist')
+        return reverse("admin:hiringapp_submission_changelist")
 
     @classmethod
     def get_all_submission(cls):
--- hiringapp/urls.py
+++ hiringapp/urls.py
@@ -3,8 +3,14 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^activity-invite/(?P<activity_uuid>[0-9a-f-]+)/$',
-        views.SubmissionInviteView.as_view(), name='submission_invite'),
-    url(r'^activity-solution/(?P<activity_uuid>[0-9a-f-]+)/$',
-        views.SubmitSolutionView.as_view(), name='submission_solution'),
+    url(
+        r"^activity-invite/(?P<activity_uuid>[0-9a-f-]+)/$",
+        views.SubmissionInviteView.as_view(),
+        name="submission_invite",
+    ),
+    url(
+        r"^activity-solution/(?P<activity_uuid>[0-9a-f-]+)/$",
+        views.SubmitSolutionView.as_view(),
+        name="submission_solution",
+    ),
 ]
