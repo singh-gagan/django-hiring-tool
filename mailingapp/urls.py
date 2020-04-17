3c47e037d036a18a69bcad5edfff6a12c956aa50
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-17 16:12:23 +0530

    applied black on mailingapp urls

--- mailingapp/urls.py
+++ mailingapp/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-    url(r'^authenticate/$', views.GmailAuthenticateView.as_view(),
-        name='gmail_authenticate'),
-    url(r'^oauth2callback/$', views.GmailAuthCallbackView.as_view(),
-        name='oauth2callback'),
+    url(
+        r"^authenticate/$",
+        views.GmailAuthenticateView.as_view(),
+        name="gmail_authenticate",
+    ),
+    url(
+        r"^oauth2callback/$",
+        views.GmailAuthCallbackView.as_view(),
+        name="oauth2callback",
+    ),
 ]
