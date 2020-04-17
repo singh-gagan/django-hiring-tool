d839be8afb761e7d84129455503a83ebc42592bc
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-17 16:11:28 +0530

    applied black on mailservices,tasks

--- mailingapp/tasks.py
+++ mailingapp/tasks.py
@@ -17,38 +17,44 @@ logger = logging.getLogger(__name__)
 def send_emails(activity_uuid, email_type):
     from hiringapp.models import Submission
 
-    logger.info('Sending {email_type} email. Actiivty UUID-{activity_uuid}'.format(
-        email_type=email_type, activity_uuid=activity_uuid))
+    logger.info(
+        "Sending {email_type} email. Actiivty UUID-{activity_uuid}".format(
+            email_type=email_type, activity_uuid=activity_uuid
+        )
+    )
 
     submission = Submission.get_submission(activity_uuid)
     email_log = EmailLog.add_new_email_log(
-        email_type, activity_uuid, submission.candidate_name, 'NOTSENT')
+        email_type, activity_uuid, submission.candidate_name, "NOTSENT"
+    )
 
     try:
         message = MailUtils.create_messages(submission, email_type)
-    except Exception as e:
+    except IndexError as error:
         logger.error(
-            '{} email not sent. Activity UUID - {}'.format(email_type, activity_uuid), exc_info=e)
+            "{} email not sent. Activity UUID - {}".format(email_type, activity_uuid),
+            exc_info=error,
+        )
         return
 
-    sent_message = GmailServices.send_message(
-        submission.invitation_host, message)
+    sent_message = GmailServices.send_message(submission.invitation_host, message)
     if sent_message is not None:
-        email_log.mail_status = 'SENT'
-        email_log.message_id = sent_message['id']
-        email_log.save(update_fields=["mail_status", "message_id", ])
+        email_log.mail_status = "SENT"
+        email_log.message_id = sent_message["id"]
+        email_log.save(update_fields=["mail_status", "message_id"])
         logger.info(
-            "{} email sent. Activity UUID - {}".format(email_type, activity_uuid))
+            "{} email sent. Activity UUID - {}".format(email_type, activity_uuid)
+        )
     else:
         logger.error(
-            '{} email not sent. Activity UUID - {}'.format(email_type, activity_uuid))
+            "{} email not sent. Activity UUID - {}".format(email_type, activity_uuid)
+        )
 
 
 @shared_task
 def checkout_pending_tasks():
     from hiringapp.models import Submission
 
-    current_date = timezone.now().date()
     reminders_gap_list = [1, 3, 6]
     all_submissions = Submission.get_all_submission()
 
@@ -70,10 +76,9 @@ def send_reminder_to_start_email(submission, reminders_gap_list):
     if current_date == latest_mail_sent_date:
         return
 
-    gap = current_date-submission.invitation_creation_dateandtime.date()
+    gap = current_date - submission.invitation_creation_dateandtime.date()
     if gap.days in reminders_gap_list:
-        send_emails.delay(submission.activity_uuid,
-                          EmailType.START_REMINDER.value)
+        send_emails.delay(submission.activity_uuid, EmailType.START_REMINDER.value)
 
 
 def send_reminder_to_submit_email(submission):
@@ -83,15 +88,14 @@ def send_reminder_to_submit_email(submission):
     if latest_mail_sent_type == EmailType.SUBMISSION_REMINDER.value:
         return
 
-    activity_reminder_time = submission.activity_end_time - \
-        submission.reminder_for_submission_time
+    activity_reminder_time = (
+        submission.activity_end_time - submission.reminder_for_submission_time
+    )
     if timezone.now() >= activity_reminder_time:
-        send_emails.delay(submission.activity_uuid,
-                          EmailType.SUBMISSION_REMINDER.value)
+        send_emails.delay(submission.activity_uuid, EmailType.SUBMISSION_REMINDER.value)
 
 
 def send_activity_expired_email(submission):
     submission.activity_status = ActivityStatus.EXPIRED.value
     submission.save(update_fields=["activity_status"])
-    send_emails.delay(submission.activity_uuid,
-                      EmailType.ACTIVITY_EXPIRED.value)
+    send_emails.delay(submission.activity_uuid, EmailType.ACTIVITY_EXPIRED.value)
