af7f3efc351ff0f834c7a0f1e6185a3a65b00da6
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-17 16:21:03 +0530

    applied black on hiringapp tests

--- hiringapp/tests/test_models.py
+++ hiringapp/tests/test_models.py
@@ -1,19 +1,17 @@
 import uuid
 from unittest.mock import patch
-
 from django.test import TestCase
 
 from hiringapp.models import Submission
 
 
 class TestModels(TestCase):
-
-    @patch('mailingapp.tasks.send_emails.delay')
+    @patch("mailingapp.tasks.send_emails.delay")
     def test_invitation_mail_generated_on_first_time_save(self, mocked_send_emails):
         Submission.objects.create()
         self.assertEqual(mocked_send_emails.call_count, 1)
 
-    @patch('mailingapp.tasks.send_emails.delay')
+    @patch("mailingapp.tasks.send_emails.delay")
     def test_get_submission(self, mocked_send_emails):
         submission = Submission.get_submission(uuid.uuid4())
         self.assertEquals(submission, None)
--- hiringapp/tests/test_views.py
+++ hiringapp/tests/test_views.py
@@ -1,72 +1,72 @@
 import uuid
 from unittest.mock import patch
 
-from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from django.urls import reverse
 from django.utils import timezone
-
 from hiringapp.constants import ActivityStatus
 from hiringapp.models import Submission
-from hiringapp.views import SubmissionInviteView, SubmitSolutionView
 
 
 class TestViews(TestCase):
-
-    @patch('mailingapp.tasks.send_emails.delay')
+    @patch("mailingapp.tasks.send_emails.delay")
     def setUp(self, mocked_send_emails):
         self.client = Client()
 
         self.submission_invite_url_with_random_uuid = reverse(
-            'submission_invite', args=(uuid.uuid4(),))
+            "submission_invite", args=(uuid.uuid4(),)
+        )
         self.submission_solution_url_with_random_uuid = reverse(
-            'submission_solution', args=(uuid.uuid4(),))
+            "submission_solution", args=(uuid.uuid4(),)
+        )
 
         self.submission = Submission.objects.create()
         self.submission_invite_url_with_valid_uuid = reverse(
-            'submission_invite', args=(self.submission.activity_uuid,))
+            "submission_invite", args=(self.submission.activity_uuid,)
+        )
         self.submission_solution_url_with_valid_uuid = reverse(
-            'submission_solution', args=(self.submission.activity_uuid,))
+            "submission_solution", args=(self.submission.activity_uuid,)
+        )
 
     def test_submission_invite_GET(self):
         response = self.client.get(self.submission_invite_url_with_random_uuid)
 
         self.assertEquals(response.status_code, 200)
-        self.assertTemplateUsed(response, 'hiringapp/activity_index.html')
+        self.assertTemplateUsed(response, "hiringapp/activity_index.html")
 
-    @patch('mailingapp.tasks.send_emails.delay')
+    @patch("mailingapp.tasks.send_emails.delay")
     def test_submission_invite_with_radom_uuid_POST(self, mocked_send_emails):
-        response = self.client.post(
-            self.submission_invite_url_with_random_uuid)
+        response = self.client.post(self.submission_invite_url_with_random_uuid)
 
         self.assertEquals(response.status_code, 200)
 
-    @patch('mailingapp.tasks.send_emails.delay')
+    @patch("mailingapp.tasks.send_emails.delay")
     def test_submission_invite_with_valid_uuid_POST(self, mocked_send_emails):
         response = self.client.post(self.submission_invite_url_with_valid_uuid)
         self.submission.refresh_from_db()
 
         self.assertEquals(response.status_code, 302)
-        self.assertEquals(self.submission.activity_status,
-                          ActivityStatus.STARTED.value)
+        self.assertEquals(self.submission.activity_status, ActivityStatus.STARTED.value)
 
     def test_submission_solution_with_random_uuid_POST(self):
         response = self.client.post(
-            self.submission_solution_url_with_random_uuid, {
-                'solution_link': 'link'})
+            self.submission_solution_url_with_random_uuid, {"solution_link": "link"}
+        )
 
         self.assertEquals(response.status_code, 200)
 
-    @patch('mailingapp.tasks.send_emails.delay')
+    @patch("mailingapp.tasks.send_emails.delay")
     def test_submission_solution_with_valid_uuid_POST(self, mocked_send_emails):
         self.submission.activity_status = ActivityStatus.STARTED.value
         self.submission.activity_start_time = timezone.now()
         self.submission.save()
 
-        response = self.client.post(self.submission_solution_url_with_valid_uuid, {
-                                    'solution_link': 'link'})
+        response = self.client.post(
+            self.submission_solution_url_with_valid_uuid, {"solution_link": "link"}
+        )
         self.submission.refresh_from_db()
 
         self.assertEquals(response.status_code, 302)
-        self.assertEquals(self.submission.activity_status,
-                          ActivityStatus.SUBMITTED.value)
+        self.assertEquals(
+            self.submission.activity_status, ActivityStatus.SUBMITTED.value
+        )
