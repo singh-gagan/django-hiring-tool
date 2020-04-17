efbce62ec37535354d5344e70f26d3608e89a6e9
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-17 16:15:41 +0530

    applied black on mysite urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('summernote/', include('django_summernote.urls')),
-    url('', include('hiringapp.urls')),
-    url('gmail/',include('mailingapp.urls'))
+    url("admin/", admin.site.urls),
+    url("summernote/", include("django_summernote.urls")),
+    url("", include("hiringapp.urls")),
+    url("mail/", include("mailingapp.urls")),
 ]
