d062e85fd220cf69a140a4f60389d8d835e150ea
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-17 16:13:21 +0530

    applied black on mailingapp views

--- mailingapp/views.py
+++ mailingapp/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponseRedirect
 from django.views import View
-
 from hiringapp.models import Submission
 
 from .mailservices import GmailServices
@@ -9,18 +8,18 @@ from .models import GmailCredential
 
 class GmailAuthenticateView(View):
     def post(self, request):
-        gmail_authorize_url = GmailServices.get_gmail_authorize_url(
-            request.user)
+        gmail_authorize_url = GmailServices.get_gmail_authorize_url(request.user)
         return HttpResponseRedirect(gmail_authorize_url)
 
 
 class GmailAuthCallbackView(View):
     def get(self, request):
-        state = bytes(request.GET.get('state'), 'utf8')
-        authorization_code = request.GET.get('code')
+        state = bytes(request.GET.get("state"), "utf8")
+        authorization_code = request.GET.get("code")
 
         credential = GmailServices.get_gmail_auth_callback_credential(
-            state, authorization_code, request.user)
+            state, authorization_code, request.user
+        )
         GmailCredential.add_credentials(request.user, credential)
 
         return HttpResponseRedirect(Submission.get_admin_change_list_url())
