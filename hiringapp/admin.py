b0ec3309db5eccdb3bd8efab9b46b82e2b196149
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-17 14:18:09 +0530

    applied black on admin

--- hiringapp/admin.py
+++ hiringapp/admin.py
@@ -13,23 +13,28 @@ from .models import Submission
 class SubmissionAdmin(admin.ModelAdmin):
 
     change_list_template = "change_list.html"
-    actions = ['cancel_flow', ]
-    list_display = ('candidate_name', 'activity_status',
-                    'invitation_creation_dateandtime', 'activity_start_time')
-    ordering = ['-invitation_creation_dateandtime']
+    actions = [
+        "cancel_flow",
+    ]
+    list_display = (
+        "candidate_name",
+        "activity_status",
+        "invitation_creation_dateandtime",
+        "activity_start_time",
+    )
+    ordering = ["-invitation_creation_dateandtime"]
 
     def changelist_view(self, request, extra_context=None):
         if not request.user.is_authenticated:
-            return HttpResponseRedirect('admin')
+            return HttpResponseRedirect("admin")
 
-        gmail_authenticated = GmailServices.is_gmail_authenticated(
-            request.user)
+        gmail_authenticated = GmailServices.is_gmail_authenticated(request.user)
         extra_context = extra_context or {}
-        extra_context['gmail_authenticated'] = gmail_authenticated
+        extra_context["gmail_authenticated"] = gmail_authenticated
 
-        return super(
-            SubmissionAdmin, self).changelist_view(
-            request, extra_context=extra_context)
+        return super(SubmissionAdmin, self).changelist_view(
+            request, extra_context=extra_context
+        )
 
     def save_model(self, request, obj, form, change):
         if not change:
@@ -46,4 +51,4 @@ class SubmissionAdmin(admin.ModelAdmin):
             submission.activity_status = ActivityStatus.EXPIRED.value
             submission.save()
 
-    cancel_flow.short_description = 'Cancel process flow'
+    cancel_flow.short_description = "Cancel process flow"
