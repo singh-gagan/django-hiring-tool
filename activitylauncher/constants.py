c8668b09955736f90bc5bdcec0410394c77779e8
gagandeepsingh-bit <gagandeep.singh@squadvoice.co>
2020-04-23 13:17:44 +0530

    added activity instructions in constants

--- activitylauncher/constants.py
+++ activitylauncher/constants.py
@@ -6,3 +6,11 @@ class ActivityStatus(Enum):
     STARTED = "started"
     SUBMITTED = "submitted"
     EXPIRED = "expired"
+
+
+ActivityInstructions = (
+    "1. The time left to submit the solution is displayed above the activity.\n"
+    + "2. You can close the activity in between however the timer will not stop\n"
+    + "3. You are allowed to submit the solution only once.\n"
+    + "4. Submit only the link to the solution which can be a github link or drive link\n"
+)
